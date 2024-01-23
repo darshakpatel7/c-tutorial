b3c20e08977b2ecf7490b8d14a819227a9de2c17
darshakpatel7 <152473666+darshakpatel7@users.noreply.github.com>
2024-01-23 17:07:01 +0530

    Add files via upload

---
+++ 9 volume/work2.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+struct marks{
+    int roll_no;
+    char name[20];
+    float Chemistry, Mathematics , Physics;
+}mark[5];
+
+int main(){
+    int n,i;
+    float Total;
+    printf("enter Student = ");  
+    scanf("%d",&n);
+
+    for ( i = 0; i < n; i++){
+        printf("Student %d\n",i+1);
+        printf("enter roll_no = \n");
+        scanf("%d",&mark[i].roll_no);
+        printf("enter name = \n");
+        scanf("%s",&mark[i].name);
+        printf("enter Chemistry mark = \n");
+        scanf("%f",&mark[i].Chemistry);
+        printf("enter Mathematics mark = \n");
+        scanf("%f",&mark[i].Mathematics);
+        printf("enter Physics mark = \n");
+        scanf("%f",&mark[i].Physics);
+    }
+
+    for ( i = 0; i < n; i++){
+        printf("\n Student %d",i+1);
+        Total=(mark[i].Chemistry + mark[i].Mathematics + mark[i].Physics)/300*100;
+        printf("\n Total is : %.2f",Total);
+    }
+    
+    return 0;
+}
---
+++ 9 volume/work3.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+enum days{
+    sunday,monday,tuesday,wednesday,thursday,friday,satruday
+};
+int main(){
+
+    // for (int i = sunday; i <= satruday; i++){
+    //     printf("%d ",i);
+    // }
+
+    //or
+
+    printf("\n sun is %d",sunday);
+    printf("\n mon is %d",monday);
+    printf("\n tue is %d",tuesday);
+    printf("\n wed is %d",wednesday);
+    printf("\n thu is %d",thursday);
+    printf("\n fri is %d",friday);
+    printf("\n sat is %d",satruday);
+
+    return 0;
+}   
